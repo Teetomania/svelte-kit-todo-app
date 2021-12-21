3bcef207b1cdc8fb0af56be0dc191d13ac308555
Tomisin Owolabi <48669572+Teetomania@users.noreply.github.com>
2021-12-21 12:17:28 +0100

    initial setup with tailwindcss

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script>
+    import TodoForm from "../components/TodoForm.svelte";
+</script>
+<main>
+    <h1 class="text-2xl font-bold text-center text-gray-800 md:text-3xl">
+        My Todos
+    </h1>
+    <TodoForm/>
+</main>
