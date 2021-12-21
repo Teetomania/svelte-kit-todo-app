d46fe25fe9ff546330f278cd66a9e29bf2e211a6
Tomisin Owolabi <48669572+Teetomania@users.noreply.github.com>
2021-12-21 14:42:49 +0100

    project done

--- src/components/TodoForm.svelte
+++ src/components/TodoForm.svelte
@@ -1,7 +1,19 @@
-<form class="my-6">
+<script>
+    import{addTodo} from "../stores/todoStore"
+    let todo = '';
+
+
+    const handleSubmit = () => {
+        addTodo(todo);
+        todo ='';
+        console.log("submitting")
+    }
+</script>
+
+<form class="my-6" on:submit|preventDefault={handleSubmit}>
     <div class="flex flex-col text-sm mb-2">
         <label class="font-bold mb-2 text-gray-800" for="todo">Todos</label>
-        <input class="appearance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg" type="text" name="todo" placeholder="What you got to do?">
+        <input bind:value={todo} class="appearance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg" type="text" name="todo" placeholder="What you got to do?">
 
     </div>
     <button type="submit" class="w-full shadow-sm rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4">Submit</button>
